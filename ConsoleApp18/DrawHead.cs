0a15acd1dd40d397a8b4ec2487a5002eaab54d2e
igromanka.igry@yandex.ru <igromanka.igry@yandex.ru>
2023-10-21 05:22:25 +0300

    last

--- ConsoleApp18/DrawHead.cs
+++ ConsoleApp18/DrawHead.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp18
             graphics.FillEllipse(System.Drawing.Brushes.Red, snake[0][0], snake[0][1], 10, 10); 
             // нарисовать красный круг размером 10 на 10 по координатам нулевой ячейки змейки
             if (snake.Count>1)
-                graphics.FillEllipse(System.Drawing.Brushes.Blue, snake[1][0], snake[1][1], 10, 10);
+                graphics.FillEllipse(System.Drawing.Brushes.Yellow, snake[1][0], snake[1][1], 10, 10);
 
         }
     }
